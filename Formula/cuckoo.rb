a6812894c36caff9b0ab25d1e5d6db879f5a1b60
circleci <noreply@borchero.com>
2020-03-26 04:45:18 +0000

    Update Cuckoo

---
+++ Formula/cuckoo.rb
@@ -0,0 +1,15 @@
+class Cuckoo < Formula
+    desc "CLI Tool for GitLab CI and Kubernetes Deployments."
+    url "https://circle-artifacts.com/gh/borchero/cuckoo/$CIRCLE_PREVIOUS_BUILD_NUM/artifacts/0/cuckoo.tar.gz"
+    sha256 "$ARTIFACT_SHA256"
+
+    depends_on "go@1.14" => :build
+
+    def install
+        bin.install "cuckoo"
+    end
+
+    test do
+        cuckoo help
+    end
+end
